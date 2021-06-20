12affbaf59cd854f8aa9c1409c4e6641782d39ed
Utkarsh Mahajan <utkarshrm568@gmail.com>
2021-06-20 12:08:20 +0530

    more bugs
    Try 4
    
    Epic Fail
    
    One more Bug
    
    FIxed Initial stuff but just one issue left.
    
    Final 1.0
    
    :_: front-end left.

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 let toDoList = [{
     text: "lorem ipsum dolor baki ka yadh nhi",
-    check: true
+    check: true,
+    deleted:false
 },{
     text: "petition to rename Anas Aijaz to Anaz Aijas",
-    check: false
+    check: false,
+    deleted:false
 }
 ];
-
-
+//dtdl is the DOm element for the ul.
 let dtdl = document.querySelector("#tdlist");
-
-
+let todoitems = document.querySelectorAll(".toDoItems");
+//adding all the existing todos
 for(i in toDoList){
+    if(toDoList[i].deleted==false){
     dtdl.appendChild(createToDo(toDoList[i]));
+    } else {
+        dtdl.appendChild(createToDo(toDoList[i]));
+        todoitems = document.querySelectorAll(".toDoItems");
+        todoitems[i].style.display = "none";
+    }
 }
 
+//function for creating to dos.
 function createToDo(tdobj){
     let litd = document.createElement("li");
     litd.classList.add('toDoItems');
@@ -40,21 +48,31 @@ function createToDo(tdobj){
 let btn =document.querySelectorAll(".btnToDo"); 
 let checkBoxes =  document.getElementsByClassName("checkBoxToDo");
 let addform = document.querySelector("#addtdform");
-let todoitems = document.querySelectorAll(".toDoItems");
+todoitems = document.querySelectorAll(".toDoItems");
 
+//event for adding to dos.
 addform.addEventListener('submit',function (e){
     e.preventDefault();
     let TodoAddObj = {
         text: `${addform.elements.textToDo.value}`,
-        check:false
+        check:false,
+        deleted:false
     }
     toDoList.push(TodoAddObj);
     dtdl.appendChild(createToDo(TodoAddObj));
-
+    addform.elements.textToDo.value="";
     //For Reassigning Event listener since elements are Changed.
+    todoitems = document.querySelectorAll(".toDoItems");
     btn = document.querySelectorAll(".btnToDo"); 
-    updatebtn();
-    updateCb();
+    checkBoxes =  document.getElementsByClassName("checkBoxToDo");
+    let j =toDoList.length;
+    btn[j-1].addEventListener('click', function(){    
+        todoitems[i].style.display = "none";
+        toDoList[j-1].deleted=true;
+    })
+    checkBoxes[j-1].addEventListener('change', function() {
+        toDoList[j-1]["check"]= checkBoxes[j-1].checked;
+    });
 })
 updatebtn();
 updateCb();
@@ -64,29 +82,20 @@ updateCb();
 function updatebtn(){
     todoitems = document.querySelectorAll(".toDoItems");
     btn = document.querySelectorAll(".btnToDo"); 
-    for(let i = 0 ; i < btn.length; i++){
-        btn[i].removeEventListener('click',function(){
-        });
-    } 
-    for(let i = 0 ; i < btn.length; i++){
+    //new listeners 
+    for(let i =0; i < btn.length; i++){
         btn[i].addEventListener('click', function(){    
-                toDoList.splice(i,1);
-                todoitems[i].remove();
-                updateCb();
+                todoitems[i].deleted=true;
+                toDoList[i].deleted=true;
+                todoitems[i].style.display = "none";
             })
     }
 }
-
-
 function updateCb(){
-    for(let i = 0 ; i < checkBoxes.length; i++){
-        checkBoxes[i].removeEventListener('change',function(){
-        });
-    } 
     checkBoxes =  document.getElementsByClassName("checkBoxToDo");
-    for(let i = 0 ; i < checkBoxes.length; i++){
-        checkBoxes[i].addEventListener('change', function() {
-                toDoList[i]["check"]= checkBoxes[i].checked;
+    for(let j = 0 ; j < checkBoxes.length; j++){
+        checkBoxes[j].addEventListener('change', function() {
+                toDoList[j]["check"]= checkBoxes[j].checked;
         });
     } 
 }
